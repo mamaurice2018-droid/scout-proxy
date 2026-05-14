d180680c2ef1d07cb35ddd8351b05891597a854a
mamaurice2018-droid <mamaurice2018@gmail.com>
2026-05-14 02:28:37 -0400

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,14 +1,18 @@
 export default async function handler(req, res) {
-  const path = req.url.replace('/api/proxy', '');
-  const url = `https://v3.football.api-sports.io${path}`;
+  res.setHeader("Access-Control-Allow-Origin", "*");
   
-  const response = await fetch(url, {
+  // Find our real outbound IP
+  const ipRes = await fetch("https://api4.ipify.org?format=json");
+  const ipData = await ipRes.json();
+  
+  // Also test the API
+  const apiRes = await fetch("https://v3.football.api-sports.io/status", {
     headers: { "x-apisports-key": "3b9924274be6dec39dede6fc8fd29b70" }
   });
+  const apiData = await apiRes.json();
   
-  const data = await response.json();
-  
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Content-Type", "application/json");
-  res.json(data);
+  res.json({ 
+    vercelOutboundIP: ipData.ip,
+    apiResponse: apiData
+  });
 }
