fb4f20b3f82261af9a3895b26973bfcf5d0645c3
mamaurice2018-droid <mamaurice2018@gmail.com>
2026-05-14 02:36:52 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,11 @@
-export default async function handler(req, res) {
+const https = require('https');
+
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   
-  // Find our real outbound IP
   const ipRes = await fetch("https://api4.ipify.org?format=json");
   const ipData = await ipRes.json();
   
-  // Also test the API
   const apiRes = await fetch("https://v3.football.api-sports.io/status", {
     headers: { "x-apisports-key": "3b9924274be6dec39dede6fc8fd29b70" }
   });
